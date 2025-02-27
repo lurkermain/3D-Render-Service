44b30d38ab637f5701a4c6939cbc4bf732b2fb47
juggernautbratan <van-pav-5@mail.ru>
2025-02-27 11:32:07 +0300

    fix adaptiv and add nginx

--- Frontend/src/components/SearchPanel.tsx
+++ Frontend/src/components/SearchPanel.tsx
@@ -1,5 +1,5 @@
-import { Input } from './ui/input';
-import { Button } from './ui/button';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 import { PlusCircle  } from 'lucide-react';
 
 interface SearchPanelProps {
@@ -16,26 +16,26 @@ export function SearchPanel({
   onCreateClick 
 }: SearchPanelProps) {
   return (
-    <div className="flex items-center justify-between space-y-2 mb-8">
-      <div className="flex items-center space-x-10">
+    <div className="flex flex-col gap-3 md:flex-row md:items-center md:justify-between space-y-4 md:space-y-2 mb-8">
+      <div className="flex flex-col sm:flex-row sm:items-center space-y-4 sm:space-y-0 sm:space-x-10">
         <div className="flex items-center space-x-6">
 
         <h2 className="text-3xl font-bold tracking-tight">Каталог товаров</h2>
       
         <span className='text-lg -mb-1'>{totalProducts}</span>
         </div>
-        <Button onClick={onCreateClick}>
+        <Button onClick={onCreateClick} className="w-full sm:w-auto">
           <PlusCircle/>
           Добавить товар
         </Button>
       </div>
-      <div className="ml-auto space-x-4">
+      <div className="flex md:ml-auto">
         <Input
           type="text"
           placeholder="Поиск..."
           value={searchQuery}
           onChange={(e) => onSearchChange(e.target.value)}
-          className="md:w-[100px] lg:w-[300px]"
+          className="w-full sm:w-[200px] md:w-[300px]"
         />
       </div>
     </div>
